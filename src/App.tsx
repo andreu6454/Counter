9a1b4c38435fa56ecabfaf24f3882cdea345064e
Andrey Malin <andreu6454@gmail.com>
2022-09-20 20:32:09 +0300

    fix problem, when maxCount < minCount

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,60 @@ import Counter from "./Components/Counter";
 import Settings from "./Components/Setting";
 
 function App() {
-    const [maxCount,setMaxCount] = useState(() => {
+    const [maxCount, setMaxCount] = useState(() => {
         let MaxCount = localStorage.getItem('maxCount')
         if (MaxCount) {
             return JSON.parse(MaxCount);
         }
         return 5
     })
-    const [minCount,setMinCount] = useState(() => {
+    const [minCount, setMinCount] = useState(() => {
         let MinCount = localStorage.getItem('minCount')
         if (MinCount) {
             return JSON.parse(MinCount);
         }
         return 0
     })
+    const [error, setError] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(true)
     const [count, setCount] = useState(minCount)
 
     useEffect(() => {
-        localStorage.setItem('minCount', JSON.stringify(minCount))
+        if (minCount < maxCount) {
+            localStorage.setItem('minCount', JSON.stringify(minCount))
+            setIsDisabled(false)
+        } else {
+            setError(true)
+            setIsDisabled(true)
+        }
+
     }, [minCount])
 
     useEffect(() => {
-        localStorage.setItem('maxCount', JSON.stringify(maxCount))
+        if (minCount < maxCount) {
+            localStorage.setItem('maxCount', JSON.stringify(maxCount))
+            setIsDisabled(false)
+        } else {
+            setError(true)
+            setIsDisabled(true)
+        }
     }, [maxCount])
 
-    const changeCount = (number: number) =>{
+    const changeCount = (number: number) => {
         setCount(number)
     }
-    const changeMaxCount = (number: number) =>{
+    const changeMaxCount = (number: number) => {
         setMaxCount(number)
     }
-    const changeMinCount = (number: number) =>{
+    const changeMinCount = (number: number) => {
         setMinCount(number)
     }
 
 
     return (
         <div className="App">
-            <Settings maxCount={maxCount} changeMaxCount={changeMaxCount} minCount={minCount} changeMinCount={changeMinCount} changeCount={changeCount}/>
+            <Settings maxCount={maxCount} changeMaxCount={changeMaxCount} minCount={minCount}
+                      changeMinCount={changeMinCount} changeCount={changeCount} error={error} isDisabled={isDisabled}/>
             <Counter count={count} changeCount={changeCount} maxCount={maxCount} minCount={minCount}/>
         </div>
     );
--- src/Components/Setting.tsx
+++ src/Components/Setting.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Button from "./Button";
 import Input from "./Input";
+import Error from "./Error";
 
 type SettingsPropsType = {
     maxCount: number,
     changeMaxCount: (number: number) => void,
     minCount: number,
     changeMinCount: (number: number) => void,
-    changeCount: (number: number) => void
+    changeCount: (number: number) => void,
+    error: boolean,
+    isDisabled: boolean
 }
 const Setting = (props: SettingsPropsType) => {
 
-    const changeHandler = () =>{
+    const changeHandler = () => {
         props.changeMaxCount(props.maxCount)
         props.changeMinCount(props.minCount)
         props.changeCount(props.minCount)
@@ -23,9 +26,10 @@ const Setting = (props: SettingsPropsType) => {
                 <Input title={"Max Value:"} value={props.maxCount} onChange={props.changeMaxCount}/>
                 <Input title={"Min Value:"} value={props.minCount} onChange={props.changeMinCount}/>
             </div>
+            <Error error={props.error} errorMessage={"Max <= Min"}/>
             <div className={"Buttons"}>
                 <div className={"SetButton"}>
-                    <Button name={"Set"} callBack={changeHandler}/>
+                    <Button name={"Set"} callBack={changeHandler} isDisabled={props.isDisabled}/>
                 </div>
             </div>
         </div>
