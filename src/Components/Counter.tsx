7444ff84c1183ee56d90d4ad7f58f3333384e362
Andrey Malin <andreu6454@gmail.com>
2022-09-20 19:42:05 +0300

    refactor Counter.tsx

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,52 +1,49 @@
-import React, {Dispatch, SetStateAction, useState} from 'react';
-import Button from "./Button";
+import React, {useState} from 'react';
 import Display from "./Display";
-import Error from "./Error";
+import Button from "./Button";
+import Error from './Error'
 
-export type colors = "White" | "Red"
-
-type CounterPropsType = {
-    count: number
-    setCount: Dispatch<SetStateAction<number>>
-    error: boolean
-    setError: Dispatch<SetStateAction<boolean>>
-    errorMessage: string
+type CounterPropsType ={
+    count: number,
+    changeCount: (number: number)=> void,
+    maxCount: number,
     minCount: number
-    maxCount: number
 }
-const Counter = (props: CounterPropsType) => {
+const Counter = (props:CounterPropsType) => {
 
-    const [isDisabledIncButton, setIsDisabledIncButton] = useState<boolean>(false)
-    const [isDisabledResetButton, setIsDisabledResetButton] = useState<boolean>(true)
-    const [color, setColor] = useState<colors>("White")
+    const [incIsDisabled,setIncIsDisabled] = useState(false)
+    const [resetIsDisabled,setResetIsDisabled] = useState(true)
+    const [color,setColor] = useState("White")
+    const [error,setError] = useState(false)
 
-
-    const incrementFunction = () => {
-        props.count + 1 === props.minCount ? setIsDisabledResetButton(true) : setIsDisabledResetButton(false)
-        if(props.count + 1 === props.maxCount) {
-            setIsDisabledIncButton(true)
+    const IncHandler = () =>{
+        if (props.count === props.maxCount - 1){
+            props.changeCount(props.count + 1)
+            setError(true)
             setColor("Red")
-            props.setError(true)
+            setIncIsDisabled(true)
+        } else{
+            props.changeCount(props.count + 1)
+            setResetIsDisabled(false)
         }
-        props.setCount(props.count + 1)
     }
-    const resetFunction = () => {
-        props.setCount(props.minCount)
-        setIsDisabledIncButton(false)
-        setIsDisabledResetButton(true)
+    const ResetHandler = () =>{
+        props.changeCount(props.minCount)
+        setError(false)
         setColor("White")
-        props.setError(false)
+        setIncIsDisabled(false)
+        setResetIsDisabled(true)
     }
 
     return (
         <div className={"Counter"}>
-            <Display count={props.count} color={color}/>
-            <Error error={props.error} errorMessage={props.errorMessage}/>
-
+            <Display color={color} count={props.count}/>
+            <Error error={error} errorMessage={"Max Value"}/>
             <div className={"Buttons"}>
-                <Button name={"Inc"} isDisabled={isDisabledIncButton} callBack={incrementFunction}/>
-                <Button name={"Reset"} isDisabled={isDisabledResetButton} callBack={resetFunction}/>
+                <Button name={"Inc"} isDisabled={incIsDisabled} callBack={IncHandler}/>
+                <Button name={"Reset"} isDisabled={resetIsDisabled} callBack={ResetHandler}/>
             </div>
+
         </div>
     );
 };
